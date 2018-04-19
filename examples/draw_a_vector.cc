93a9d531ef3ee0c690b472a5876bbd2487e6fb9d
Sleeplessy <sleeplessy@mailbox.org>
2018-04-19 20:58:42 +0800

    Add specific colors to current operating elem

--- examples/draw_a_vector.cc
+++ examples/draw_a_vector.cc
@@ -18,37 +18,83 @@
 */
 
 #include "../visualize.hpp"
-#include <iostream>
 #include <vector>
 #include <SDL2pp/Window.hh>
-
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <random>
+#include <future>
 
 using namespace SDL2pp;
 
+std::vector<int> random_list(unsigned size = 100) {
+    std::random_device rnd_device;
+    // Specify the engine and distribution.
+    std::mt19937 mersenne_engine(rnd_device());
+    std::uniform_int_distribution<int> dist(1, 52);
+
+    auto gen = std::bind(dist, mersenne_engine);
+    std::vector<int> vec(size);
+    generate(vec.begin(), vec.end(), gen);
+    return vec;
+}
+
 int main(int, char *[]) try {
+
     SDL sdl(SDL_INIT_VIDEO);
-    Window window("libSDL2pp demo: sprites", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
+    Window window("simple-visualization insert sort", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 900, 600,
                   SDL_WINDOW_RESIZABLE);
     Renderer render(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
     render.SetDrawBlendMode(SDL_BLENDMODE_BLEND);
-    std::vector<int> v{1, 3, 3, 4, 5, 11, 7, 8, 17}; // init a data array
+    render.SetDrawColor(0, 0, 0);
+    render.Clear();
+    render.Present();
+
+    std::condition_variable cv;
+    std::condition_variable cv_update;
+    std::condition_variable cv_start;
+    std::mutex m;
+    std::mutex m2;
+    std::unique_lock<std::mutex> lk(m);
+    std::unique_lock<std::mutex> start(m2);
+    std::vector<int> v = random_list(120); // init a data array
+    std::size_t current = 0;
+    std::thread t([&]() {
+                      while (1) {
+                          cv.wait(lk);
+                          visualize::drawList<8, 8>(v, render, current); // draw it!
+                          render.Present();
+                          cv_update.notify_one();
+                          std::this_thread::yield();
+                      }
+                  }
+    );
+    std::thread t1([&v, &cv, &current, &lk, &cv_start, &start]() { // sorting thread
+        for (auto i = v.begin(); i != v.end(); ++i) {
+            std::rotate(std::upper_bound(v.begin(), i, *i), i, i + 1); // insertion sort
+            current = std::distance(v.begin(), i);
+            cv.notify_one();
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
+        }
+        cv.notify_all();
+    });
+    t.detach();
+    t1.detach();
     while (1) {
-        // Process input
         SDL_Event event;
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT ||
                 (event.type == SDL_KEYDOWN && (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_q)))
                 return 0;
-
-        // Clear screen
-        render.SetDrawColor(0, 32, 32);
+        }
+        cv_update.wait(lk);
+        render.SetDrawColor(0, 0, 0);
         render.Clear();
-        visualize::drawList(v,render); // draw it!
-        render.Present();
-        // Frame limiter
         SDL_Delay(1);
     }
+
 
     return 0;
 } catch (std::exception &e) {
--- visualize.hpp
+++ visualize.hpp
@@ -1,7 +1,7 @@
 #ifndef __DRAW_ELEM__
 #define __DRAW_ELEM__
 
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <SDL2pp/SDL.hh>
 #include <SDL2pp/Renderer.hh>
 #include <iterator>
@@ -24,16 +24,28 @@ namespace visualize {
 
 
     template<std::size_t scale_x = 6, std::size_t scale_y =6,typename T,template <typename, typename> class Container>
-    void drawList(Container<T,std::allocator<T>> elem_list, SDL2pp::Renderer &render) {
+    void drawList(Container<T, std::allocator<T>> elem_list, SDL2pp::Renderer &render, std::size_t current = 0,
+                  const SDL2pp::Color cur_color = {255, 153, 51},
+                  const SDL2pp::Color default_color = {6, 202, 72}) // Colors
+    {
         std::for_each(std::begin(elem_list), std::end(elem_list),
-                      [scale_x = scale_x, scale_y = scale_y,iter = std::begin(elem_list), &elem_list, &render]
+                      [scale_x = scale_x, scale_y = scale_y, iter = std::begin(
+                              elem_list), &elem_list, &render, current, cur_color, default_color]
                               (T n) mutable {
-                          drawListElement(n * scale_y * 0.125, std::distance(std::begin(elem_list), iter++),
+                          unsigned cur_distance = std::distance(std::begin(elem_list), iter++);
+                          !std::is_sorted(std::begin(elem_list), std::end(elem_list)) ?
+                          drawListElement(n * scale_y * 0.125, cur_distance,
                                           std::distance(std::begin(elem_list),std::end(elem_list)) / (scale_x * 0.125), // scale for width
-                                          *std::max_element(std::begin(elem_list),std::end(elem_list)), render);
+                                          *std::max_element(std::begin(elem_list), std::end(elem_list)), render,
+                                          (current == cur_distance) ? cur_color : default_color
+                          ) : drawListElement(n * scale_y * 0.125, cur_distance,
+                                              std::distance(std::begin(elem_list), std::end(elem_list)) /
+                                              (scale_x * 0.125), // scale for width
+                                              *std::max_element(std::begin(elem_list), std::end(elem_list)), render,
+                                              default_color
+                          );
                       }
         );
-        render.Present();
     }
 }
 #endif
